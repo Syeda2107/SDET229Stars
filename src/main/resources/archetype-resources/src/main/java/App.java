bedcacbe36be9ea415ec65d510a4a650f2316501
Gayu2894 <Gayathrisekar041328@gmail.com>
2025-11-18 20:04:30 +0530

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $DS-AlgoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
